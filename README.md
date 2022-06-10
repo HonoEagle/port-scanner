33dcda6b6d5a79f059f9a581edd479b1dc57fa10
HonoEagle <honoeagle@outlook.be>
2022-06-11 00:22:21 +0200

    import all files

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# port-scanner
- 
+# Port scanner
+
+Scan port on a ip address and return the result.
+
+### Prerequisites
+
+The things you need before installing the software.
+
+* python3.x
+
+### Installation
+
+
+```
+$ pip3 install -r requierements.txt
+```
+
+## Usage
+
+
+```
+py wifiscanner.py <ip> (on Windows)
+pyhton3 wifiscanner.py <ip> (on Linux)
+```
+
+## Example
+
+```
+py wifiscan.py 10.0.0.10
+```
+```
+python3 wifiscan.py 10.0.0.10
+```
---
+++ wifiscan.py
@@ -0,0 +1,88 @@
+## Under construction
+
+from random import randint, shuffle
+import sys
+import time
+from traceback import print_tb
+import pyfiglet
+import socket
+from datetime import datetime
+
+from pystyle import *
+
+banner1 = r'''
+  ___         _     ___                            
+ | _ \___ _ _| |_  / __| __ __ _ _ _  _ _  ___ _ _ 
+ |  _/ _ \ '_|  _| \__ \/ _/ _` | ' \| ' \/ -_) '_|
+ |_| \___/_|  \__| |___/\__\__,_|_||_|_||_\___|_|  
+                                                   '''[1:]
+
+yellow = Col.StaticMIX([Col.blue, Col.yellow])
+
+def stage(text: str, symbol: str = '...') -> str:
+    yyellow = yellow if symbol == '...' else Col.light_blue
+    return f""" {Col.Symbol(symbol, yyellow, Col.blue)} {yyellow}{text}{Col.reset}"""
+    
+
+
+
+def main():
+    System.Size(150, 40)
+    System.Title("Port Scanner")
+    print()
+    print(Colorate.Diagonal(Colors.purple_to_blue, Center.XCenter(banner1 + '\n\n')))
+    print('\n')
+    print(stage("Welcome to port Scanner", '-'))
+    time.sleep(1)
+    time.sleep(1)
+    print(stage("Scanning on" + target , '-'))
+    time.sleep(1)
+    print('\n')
+    print('[+] Scan complete!')
+    print('\n')
+    print('[+] Found:')
+    print('\n')
+    print('[+] Wifi Scanner')
+    print('\n')
+    input("Press Enter to exit...")
+    exit()
+
+if len(sys.argv) == 2:
+    target = socket.gethostbyname(sys.argv[1])
+else:
+    print("Invalid amount of Argument")
+    print("Syntax (linux): python3 wifiscan.py <ip>")
+    print("Syntax (windows): py wifiscan.py <ip>")
+    exit()
+try:
+    System.Size(150, 40)
+    System.Title("Port Scanner")
+    print(Colorate.Diagonal(Colors.purple_to_blue, Center.XCenter(banner1 + '\n\n')))
+    print(stage("Scanning " + target , '-'))
+    print(stage(f"Scan started at {str(datetime.now())}"))
+    time.sleep(1)
+    for port in range(1,65535):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(0.01)
+        result = s.connect_ex((target,port))
+        if result ==0:
+            print(stage(Col.green + "Port " + str(port) + f" is open on {target}" + Col.reset))
+        s.close()
+    print('\n')
+    print(stage("Scanning complete!", '-'))
+    print(stage("Scan ended at " + str(datetime.now())))
+    input(stage("\nPress Enter to exit..."))
+    
+except KeyboardInterrupt:
+        print("\n")
+        print(f""" {Col.Symbol('!', Col.light_red, Col.blue)} {Col.light_red}KeyboardInterrupt Detected!{Col.reset}""")
+        time.sleep(1)
+        print(f""" {Col.Symbol('!', Col.light_red, Col.blue)} {Col.light_red}Exiting...{Col.reset}""")
+        sys.exit()
+except socket.gaierror:
+        print("\n Hostname Could Not Be Resolved !!!!")
+        sys.exit()
+except socket.error:
+        print("\ Server not responding !!!!")
+        sys.exit()
+
